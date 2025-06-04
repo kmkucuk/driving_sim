63f000a828bdb5113d18bce7b93b10b2fd36ccc9
Kurtuluş Mert Küçük <48620003+kmkucuk@users.noreply.github.com>
2025-06-04 21:55:09 +0300

    changed psychopy file to accommodate changes

--- experimental_parameters.py
+++ experimental_parameters.py
@@ -178,7 +178,7 @@ all_widgets = [
                     {"temperature": {"text": "", "position_percentage": [40, 50], "position_pixel": []}                   
                      },
                  "image_components": 
-                 {"temperature": {"file":  getImageWithKeyword("./stimuli/clutter", "garage"), 
+                 {"temperature": {"file":  getImageWithKeyword("./stimuli/clutter", "temperature"), 
                                   "position_percentage": [50, 50], "position_pixel": [],
                                "size_ratio": [0.8, 0.8], "size_pixel": []}
                   },
@@ -192,7 +192,7 @@ all_widgets = [
                      "miles": {"text": "", "position_percentage": [60, 60], "position_pixel": []}
                      },
                  "image_components": 
-                 {"temperature": {"file":  getImageWithKeyword("./stimuli/clutter", "garage"), 
+                 {"battery": {"file":  getImageWithKeyword("./stimuli/clutter", "battery"), 
                                   "position_percentage": [50, 50], "position_pixel": [],
                                "size_ratio": [0.8, 0.8], "size_pixel": []}
                   },
@@ -228,189 +228,14 @@ for index in range(0, len(all_widgets)):
         all_widgets[index]["image_components"][key]["size_pixel"] = getIconSize(widget_regions[widget_index], val["size_ratio"])
 
 
-
-a=0
-
-left_symbol_x = [2, 10]
-symbol_y_parse = [0, 3]
-
-centered_symbol_x = [1, 3]
-centered_symbol_y = [1, 3]
-
-# 
-text_x_parse = [3, 10]
-text_y_parse = [0, 3]
-
-
-temp_text_x = [5, 20]
-temp_text_y = [8, 20]
-
-battery_text_x = [2, 20]
-battery_text_y = [8, 20]
-
-miles_text_x = [6, 20]
-miles_text_y = [15, 20]
-
-temperature_int = 96
-battery_int = 24
-miles_int = 78
-temperature_text = ('').join([str(temperature_int), "F"])
-battery_text = ('').join(["%", str(battery_int)])
-miles_text = ('').join([str(miles_int), "mi"])
-
-trip_widget = {"info": {"parse_symbol": [left_symbol_x, symbol_y_parse], "parse_text": [text_x_parse, text_y_parse], "widget_index": 0, "section_count": 4},                
-               "components": {"header": "Current Trip",
-                            "duration": "1 hr 53 min", 
-                            "fuel": "34 mpg", 
-                            "distance": "78 mi"},
-                "img_files": {},
-                "sym_component_positions": {},
-                "txt_component_positions": {}
-                            } # todo: add variable parameters to these text
-
-
-garage_widget = {"info": {"parse_symbol": [left_symbol_x, symbol_y_parse], 
-                          "parse_text": [text_x_parse, text_y_parse], 
-                          "widget_index": 1, "section_count": 2}, 
-                 "components": {"header": "My Home",
-                                "garage": "Garage Door\nOpen"},
-                "img_files": {},
-                "sym_component_positions": {},
-                "txt_component_positions": {}
-                }
-
-
-temperature_widget = {"info": {"parse_symbol": [centered_symbol_x, centered_symbol_y], "parse_text": [temp_text_x, temp_text_y], "widget_index": 3, "section_count": 1}, 
-              "components": {"temperature": temperature_text},
-                "img_files": {},
-                "sym_component_positions": {},
-                "txt_component_positions": {}
-               }
-
-battery_widget = {"info": {"parse_symbol": [centered_symbol_x, centered_symbol_y], "parse_text": [text_x_parse, text_y_parse], "widget_index": 4, "section_count": 1}, 
-                 "components": {"battery": battery_text,
-                                "miles": miles_text},
-                "img_files": {},
-                "sym_component_positions": {},
-                "txt_component_positions": {}
-                }
-
-day_widget = {"info": {"parse_symbol": [centered_symbol_x, centered_symbol_y], "parse_text": [text_x_parse, text_y_parse], "widget_index": 5, "section_count": 2}, 
-                 "components": {"header": "day",
-                                "calendar": None},
-                "img_files": {},
-                "sym_component_positions": {},
-                "txt_component_positions": {}
-                }
-
-all_widgets = [trip_widget, garage_widget, temperature_widget, battery_widget, day_widget]
-
-# first assume header to be the same size in terms of roi
-# then we can adjust it to become something else (smaller then etc.)
-def segment_roi(panel, roi, widget_dict):
-    
-    # garage widget (duration, fuel, distance)
-
-    component_count = widget_dict["info"]["section_count"]
-    component_regions = panel.panel_separator(roi[widget_dict["info"]["widget_index"]], component_count, "vertical")
-    iteration = 0
-    widget_dict["component_regions"] = component_regions
-    # garage widget (duration, fuel, distance)
-
-    for key, comp_val in widget_dict["components"].items():         
-        placement_desc = None
-        if key != "header":
-            # get symbol positions             
-            widget_dict["sym_component_positions"][key] = [panel.get_clutter_coordinate(component_regions[iteration], "horizontal", widget_dict["info"]["parse_symbol"][0]), 
-            panel.get_clutter_coordinate(component_regions[iteration], "vertical", widget_dict["info"]["parse_symbol"][1])]
-            widget_dict["img_files"][key] = panel.getImageWithKeyword("./stimuli/clutter", key)
-           
-        if key == "header":
-            text_x_header = [1, 10]
-            text_y_header = [2, 7]     
-            
-            if comp_val == "day":
-                # get current day for text display on calemdar widget
-                my_date = date.today()
-                today = datetime.now()
-                month_index = datetime.now().month
-                month = calendar.month_name[month_index]
-                text_x_header = [1, 10]
-                text_y_header = [2, 7]
-                numeric_day = today.day # this is a integer
-                str_num_day = str(numeric_day)
-                if str_num_day[-1] == '1':
-                    day_suffix = 'st'
-                elif str_num_day[-1] == '2':
-                    day_suffix = 'nd'
-                elif str_num_day[-1] == '3':
-                    day_suffix = 'rd'
-                elif int(str_num_day[-1]) >= 4:
-                    day_suffix = 'th'                
-                
-                date_text =  str_num_day + day_suffix + " " + month +"\n" + calendar.day_name[my_date.weekday()] # e.g. 'Wednesday'\nNumber         
-                widget_dict["components"]["header"] =  str_num_day + day_suffix + " " + month +"\n" + calendar.day_name[my_date.weekday()] # e.g. 'Wednesday'\nNumber         
-                
-            # headers are more leftward than regular text, hence specific text parsing
-            widget_dict["txt_component_positions"][key] = [panel.get_clutter_coordinate(component_regions[iteration], 
-                                                                                        "horizontal", text_x_header), 
-                                                           panel.get_clutter_coordinate(component_regions[iteration], 
-                                                                                        "vertical", text_y_header)]
-            
-        elif key != "header" and isinstance(comp_val, str):
-            placement_desc = 'bottom'
-            if comp_val == "battery":
-                text_x_parse = battery_text_x
-                text_y_parse = battery_text_y
-            elif comp_val == "miles":
-                text_x_parse = miles_text_x
-                text_y_parse = miles_text_y
-            else:
-                text_x_parse = widget_dict["info"]["parse_text"][0]
-                text_y_parse = widget_dict["info"]["parse_text"][1]
-            widget_dict["txt_component_positions"][key] = [panel.get_clutter_coordinate(component_regions[iteration], "horizontal", text_x_parse, placement_desc), 
-            panel.get_clutter_coordinate(component_regions[iteration], "vertical", text_y_parse)]
-        
-        if component_count == len(widget_dict["components"].items()):
-            iteration = iteration + 1
-
-    return widget_dict
-
-trip_widget = segment_roi(panel_layout, widget_regions, trip_widget)
-garage_widget = segment_roi(panel_layout, widget_regions, garage_widget)
-day_widget = segment_roi(panel_layout, widget_regions, day_widget)
-temperature_widget = segment_roi(panel_layout, widget_regions, temperature_widget)
-battery_widget = segment_roi(panel_layout, widget_regions, battery_widget)
-
-trip_widget["sym_component_positions"]["duration"]
-trip_widget["sym_component_positions"]["fuel"]
-trip_widget["sym_component_positions"]["distance"]
-
-print(widget_regions)
-
-print('trip_widget\n', json.dumps(trip_widget, indent=2))
-# print('\n\ngarage_widget\n',json.dumps(garage_widget, indent=2))
-# print('\n\ntemperature_widget\n',json.dumps(temperature_widget, indent=2))
-# print('\n\nbattery_widget',json.dumps(battery_widget, indent=2))
-print('\n\nday_widget\n',json.dumps(day_widget, indent=2))
-
-garage_icon_size = [garage_widget["component_regions"][1]["width"] * 0.35, garage_widget["component_regions"][1]["width"] * 0.35]
-onethird_icon_size = [trip_widget["component_regions"][0]["width"] * 0.3125, trip_widget["component_regions"][0]["width"] * 0.3125]
-half_icon_size = [day_widget["component_regions"][1]["width"] * 0.8, day_widget["component_regions"][1]["width"] * 0.8]
-full_icon_size = [temperature_widget["component_regions"][0]["width"] * 0.8, temperature_widget["component_regions"][0]["width"] * 0.8]
+# letter size is dependent on the large panel's height
+letter_size = widget_regions[1]["height"] * 0.2
+day_size = widget_regions[1]["width"] * 0.125
+header_size = widget_regions[1]["width"]  * 0.1
+temperature_size = widget_regions[1]["width"]  * 0.14
+battery_size = widget_regions[1]["width"] * 0.12
 
 textFont = "./stimuli/font/robotoflex.ttf"
 
-text_size = trip_widget["component_regions"][0]["width"]/12
-# choose a targel panel (1 to 10)
-# Panel Numbers: [1, [2/3], [4/5], 6]
-
-# letter size is dependent on the large panel's height
-letter_size = temperature_widget["component_regions"][0]["height"] * 0.2
-day_size = day_widget["component_regions"][0]["width"] * 0.125
-header_size = trip_widget["component_regions"][0]["width"] * 0.1
-temperature_size = temperature_widget["component_regions"][0]["width"] * 0.14
-battery_size = battery_widget["component_regions"][0]["width"] * 0.12
-
-
+text_size = widget_regions[1]["width"]/12
 header_wrap_width = 500
