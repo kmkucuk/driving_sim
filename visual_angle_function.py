1ceade9727bfc5e2c92791327d9002de7b77e3e0
Kurtuluş Mert Küçük <48620003+kmkucuk@users.noreply.github.com>
2025-07-08 17:22:24 +0300

    changed obs distance to 30 from 22 inches, updated stimulus size accordingly

--- visual_angle_function.py
+++ visual_angle_function.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
 
 inch_vert_monitor_lab = 11.6875
-cm_vert_monitor_lab = 29.68625
+cm_vert_monitor_lab = 29.21
 cm_vert_monitor_mert = 33.5
 monitor_res_lab = [1920, 1080]
 
@@ -9,15 +9,15 @@ pix_per_cm_lab = monitor_res_lab[1] / cm_vert_monitor_lab
 
 pix_per_cm_mert = monitor_res_lab[1] / cm_vert_monitor_mert
 
-distance_cm = 55.88
+distance_cm = 76.2
 
-pix_per_cm_lab = 36.381
-pix_per_cm_mert = 32.381
+# pix_per_cm_lab = 36.381
+# pix_per_cm_mert = 32.381
 char_size_arc_min = 19.6
 char_size_degree = 0.327
 
-stim_cm = 0.3189
-stim_pix = pix_per_cm_mert * stim_cm
+stim_cm = 0.4349
+stim_pix = pix_per_cm_lab * stim_cm
 
 print(pix_per_cm_lab)
 print(pix_per_cm_mert)
