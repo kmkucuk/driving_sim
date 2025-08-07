4538ac5eb42ad04643916e5f6c84b6fda1f162d0
Kurtuluş Mert Küçük <48620003+kmkucuk@users.noreply.github.com>
2025-08-07 19:32:28 +0300

    eurostile added for final clutter exp, final exp modified to accommodate changes in no clutter

--- visual_angle_function.py
+++ visual_angle_function.py
@@ -28,17 +28,22 @@ pix_for_required_inch = 31.21152
 # get pixel size to x-height ratio.
 # then use this ratio to multiply letter height in Psychopy. 
 # then you get the desired x-height in real units (cm or inches). 
-neueFont = TTFont('georgia.ttf')
+# neueFont = TTFont('georgia.ttf')
+# x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
+# print(stim_pix / x_height)
+
+# neueFont = TTFont('robotoflex.ttf')
+# x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
+# print(stim_pix / x_height)
+
+# neueFont = TTFont('neuefrutigerworld.ttf')
+# x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
+# print(stim_pix / x_height)
+
+neueFont = TTFont('eurostile.TTF')
 x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
 print(stim_pix / x_height)
 
-neueFont = TTFont('robotoflex.ttf')
-x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
-print(stim_pix / x_height)
-
-neueFont = TTFont('neuefrutigerworld.ttf')
-x_height =((neueFont['glyf']['x'].yMax - neueFont['glyf']['x'].yMin )/ neueFont['head'].unitsPerEm ) * stim_pix
-print(stim_pix / x_height)
 
 
 a=0
